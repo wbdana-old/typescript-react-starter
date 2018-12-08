43ed8ea4ce253498ec542b56a3e52d1a64b1d784
William Dana <william.b.dana@gmail.com>
2018-12-08 00:02:36 -0500

    Complete tasks up to 'Adding state management'

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import StatefulHello from './components/StatefulHello';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  <StatefulHello name={"TypeScript"} enthusiasmLevel={10} />,
   document.getElementById('root') as HTMLElement
+  // getElementById returns null if 'root' not present
+  // We assume getElementById will succeed, so we need to convince TypeScript of that using as syntax
+  // Could also use document.getElementById('root')! which removes null and undefined from prior expression
 );
 registerServiceWorker();
